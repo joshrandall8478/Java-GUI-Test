c715f07e693d552401df775281e452f179ea4ad3
josh <joshrandall8478@gmail.com>
2023-01-16 23:57:04 -0500

    finished tutorial

--- Driver.java
+++ Driver.java
@@ -1,5 +1,9 @@
+/*
+ * THIS IS NOT MY ORIGINAL CODE, JUST ME LEARNING HOW JAVA GUI/WINDOWS WORK.
+ * REFERENCE TUTORIAL: https://www.youtube.com/watch?v=HgkBvwgciB4
+ */
 public class Driver{
     public static void main(String[] args) {
-        
+        LaunchPage launchPage = new LaunchPage();
     }
 }
